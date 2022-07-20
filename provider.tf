cc167757a9dbe348af74889921f61c6b907baba5
chaithu117 <109440487+chaithu117@users.noreply.github.com>
2022-07-20 16:56:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,4 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "3.74.0"
-#     }
-#   }
-# }
-
 provider "aws" {
   region  = "us-east-1"
-  profile = "profile-with-no-valid-creds"
 }
 
-data "aws_caller_identity" "current" {}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
