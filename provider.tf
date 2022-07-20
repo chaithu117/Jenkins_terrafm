40fca78658beba3fee460767c1ec5a276b6fccf2
chaithu117 <109440487+chaithu117@users.noreply.github.com>
2022-07-20 16:40:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,5 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "profile-with-no-valid-creds"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
